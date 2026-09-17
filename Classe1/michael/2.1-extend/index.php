0d0e19b91888a6ac07545a177f16cdd145129f70
WebDevCF2m <gitweb@cf2m.be>
2026-09-17 16:25:23 +0200

    2.1 à 2.4

--- Classe1/michael/2.1-extend/index.php
+++ Classe1/michael/2.1-extend/index.php
@@ -1,5 +1,8 @@
 <?php
-require_once "Animal.php"
+// nécessaire avant chien
+require_once "Animal.php";
+// classe extend de Animal
+require_once "Chien.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,11 +12,32 @@ require_once "Animal.php"
     <title>Accueil</title>
 </head>
 <body>
+    <pre><code>$animal = new Animal("LaChose");
+echo "Mon animal se nomme ".$animal->getName();
+echo $animal->manger();</code></pre>
     <?php
-    $chien = new Animal("Rex<br>");
-    echo "Mon animal se nome ".$chien->getName();
-
-    var_dump($chien);
+    $animal = new Animal("LaChose");
+    echo "Mon animal se nomme ".$animal->getName();
+    echo "<br>".$animal->manger();
+    // 2.4
+    echo "<br>".$animal->crier();
+    // non possible echo $animal->aboyer();
+?><br>
+    <pre><code>$chien = new chien("Rex");
+echo "Mon chien se nomme ".$chien->getName();
+echo $chien->manger();
+echo $chien->aboyer();</code></pre>
+    <?php
+    $chien = new Chien("Rex");
+    echo "Mon chien se nomme ".$chien->getName();
+    echo "<br>".$chien->manger();
+    echo "<br>".$chien->aboyer();
+    // 2.4
+    echo "<br>".$chien->crier();
+?><br>
+<hr>
+<?php
+    var_dump($animal,$chien);
     ?>
 </body>
 </html>
